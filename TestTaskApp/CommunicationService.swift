5b1296ef62927b01f4a886ab86be564212472f1e
Petar Santor <pero.santor@gmail.com>
2017-03-27 15:21:20 +0200

    Robot font added

---
+++ TestTaskApp/CommunicationService.swift
@@ -0,0 +1,9 @@
+//
+//  CommunicationService.swift
+//  TestTaskApp
+//
+//  Created by Petar Santor on 3/27/17.
+//  Copyright © 2017 Petar Santor. All rights reserved.
+//
+
+import Foundation
---
+++ TestTaskApp/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  TestTaskApp
+//
+//  Created by Petar Santor on 3/27/17.
+//  Copyright © 2017 Petar Santor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct Font {
+        static let RobotoLight = "Roboto-Light"
+        static let RobotoRegular = "Roboto-Regular"
+        static let RobotoMedium = "Roboto-Medium"
+    }
+}
