d64df30ffc2e80a1183efeba4d5da90f8eccece8
Petar Santor <pero.santor@gmail.com>
2017-03-27 14:03:01 +0200

    Move fields when showing/hiding keyboard

--- TestTaskApp/LogInViewController.swift
+++ TestTaskApp/LogInViewController.swift
@@ -8,18 +8,113 @@
 
 import UIKit
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, UITextFieldDelegate {
 
+    
+    //MARK: - Outlets
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    
+    @IBOutlet weak var buttonLogIn: UIButton!
+    
+    
+    //MARK: - Lifecycle
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setUp(textField: self.textFieldEmail)
+        self.setUp(textField: self.textFieldPassword)
+        
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(self.dismissKeyboard(gesture:)))
+        self.view.addGestureRecognizer(tapGesture)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerForKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.deregisterFromKeyboardNotifications()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    //MARK: - Utilities
+    
+    
+    func setUp(textField field:UITextField?) {
+        field?.delegate = self
 
+        field?.autocorrectionType = .no
+        field?.spellCheckingType = .no
+        
+        if (field == self.textFieldEmail) {
+            field?.returnKeyType = .next
+            field?.keyboardType = .emailAddress
+        } else {
+            field?.returnKeyType = .done
+            field?.isSecureTextEntry = true
+        }
+    }
+    
+    func dismissKeyboard(gesture: UITapGestureRecognizer) {
+        self.textFieldEmail.resignFirstResponder()
+        self.textFieldPassword.resignFirstResponder()
+    }
+    
+    func registerForKeyboardNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func deregisterFromKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification){
+        //Need to calculate keyboard exact size due to Apple suggestions
+        self.scrollView.isScrollEnabled = true
+        var info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height + 20, 0.0)
+        
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect : CGRect = self.view.frame
+        aRect.size.height -= keyboardSize!.height
+        if let activeField = self.buttonLogIn {
+            if (!aRect.contains(activeField.frame.origin)){
+                self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+            }
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification){
+        //Once keyboard disappears, restore original positions
+        var info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, -keyboardSize!.height - 20, 0.0)
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        self.view.endEditing(true)
+        self.scrollView.isScrollEnabled = false
+    }
+    
 
 }
 
